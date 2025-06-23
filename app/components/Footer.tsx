01b4f564163082dbc088e08d47c4f0ae7ef90a2e
JoleneKearse <pharaohnutz@gmail.com>
2025-06-23 15:42:50 -0400

    chore: fix footer position with fixed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,7 +8,7 @@ import { FaCalendarDays } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer className="sticky bottom-0 w-full bg-linear-[270deg,#dc2626,#ea580c,#eab308,#16a34a,#0284c7,#7c3aed,#c026d3,#e11d48]">
+    <footer className="fixed bottom-0 w-full bg-linear-[270deg,#dc2626,#ea580c,#eab308,#16a34a,#0284c7,#7c3aed,#c026d3,#e11d48]">
       <nav className="flex items-center justify-center gap-4 p-6 dark:border-gray-700">
         <ul className="list-none flex items-center gap-12">
           <li>
