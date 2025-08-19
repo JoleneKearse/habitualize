5655434cf70347ca5d565a8b979188cd0d5b6b0c
JoleneKearse <pharaohnutz@gmail.com>
2025-08-19 15:54:57 -0400

    chore: fix spelling error

--- app/routes/logIn.tsx
+++ app/routes/logIn.tsx
@@ -114,7 +114,7 @@ export default function Login() {
         value="register"
         className="px-6 py-2 mt-4 bg-linear-[90deg,#dc2626,#ea580c,#eab308,#16a34a,#0284c7,#7c3aed,#c026d3,#e11d48] text-gray-50 text-4xl font-bold rounded-2xl hover:bg-linear-[90deg,#e11d48,#c026d3,#7c3aed,#0284c7,#16a34a,#eab308,#ea580c,#dc2626] duration-800 ease-in-out cursor-pointer"
       >
-        Resister
+        Register
       </button>
     </Form>
   );
