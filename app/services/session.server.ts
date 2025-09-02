34749c8fa511cd9fa2941a475c1808ea886422ec
JoleneKearse <pharaohnutz@gmail.com>
2025-09-02 15:52:44 -0400

    chore: check if session secret set

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,5 +1,9 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 
+if (!process.env.SESSION_SECRET) {
+  throw new Error("SESSION_SECRET is not set");
+}
+
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__session",
